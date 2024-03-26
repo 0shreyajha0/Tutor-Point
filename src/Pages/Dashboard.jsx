ea774d9c2c462bd950ee14e4bbdd9ce621c3e343
0shreyajha0 <0shreyajha0@gmail.com>
2024-03-26 19:43:50 +0530

    added a button for navigation from footer to header

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -3,14 +3,31 @@ import { TutorReview } from "../components/TutorReview";
 import { Header } from "../components/Header";
 import { TutorProfile } from "../components/TutorProfile";
 import { LearnContent } from "../components/LearnContent";
+//import { AvailableCity } from "../components/AvailableCity";
+import Fab from "@mui/material/Fab";
+import NavigationIcon from "@mui/icons-material/Navigation";
+
 export const Dashboard = () => {
   return (
     <div>
       <Header />
+
       <TutorProfile />
       <TutorReview />
       <LearnContent />
+      <Fab variant="extended">
+        <NavigationIcon sx={{ mr: 1 }} />
+        Navigate
+      </Fab>
       <Footer />
     </div>
   );
 };
+
+// const arr=[
+
+//   : 'TutorProfile',
+//   place arr=[ French,,
+//     Mathematics,
+//     Chemistry,];
+// ]
